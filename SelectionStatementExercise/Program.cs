8dbdc6c71368b709da434bf0e5f997a9842674b1
tyson lind <tysonfloydlind@gmail.com>
2023-04-03 09:40:03 -0600

    completed exercises

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("You guessed my favorite number!");
+            } else if (userInput > favNumber)
+            {
+                Console.WriteLine("Your guess was too high");
+            } else if (userInput < favNumber)
+            {
+                Console.WriteLine("Your guess was too low");
+            } else
+            {
+                Console.WriteLine("Invalid entry");
+            }
         }
     }
 }
